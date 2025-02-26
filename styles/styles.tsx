927332e96127a05a98ea6f6f08facf3b0cf1547e
ethanatique <65488662+ethanatique@users.noreply.github.com>
2025-02-26 23:37:49 +0100

    espacement historique

--- styles/styles.tsx
+++ styles/styles.tsx
@@ -12,7 +12,6 @@ export const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: 'bold',
     color: '#444',
-    marginBottom: 20,
     textShadowColor: 'rgba(0, 0, 0, 0.2)',
     textShadowOffset: { width: 1, height: 1 },
     textShadowRadius: 2,
@@ -66,6 +65,7 @@ export const styles = StyleSheet.create({
     elevation: 5,
     width: "90%",
     alignSelf: 'center',
+    marginTop: 20,
   },
   nomPlante: {
     fontSize: 24,
